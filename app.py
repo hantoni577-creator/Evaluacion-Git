93462cdc4e0593f8c0a1b1fcd44382a1814152ec
Alejandraa <ramirezpazalejandra26@gmail.com>
2026-02-13 17:09:13 -0500

    implementa suma y mayor

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 def analizar(lista):
-    pass
+    suma= sum(lista)
+    mayor=+(lista)
+    return {
+        "suma":suma,
+        "mayor": mayor
+    }
 
     if __name__ == "__main__":
         numeros = [4, 8, 2, 10]
